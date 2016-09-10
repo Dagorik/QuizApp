016dbd23b522aaf1d57091fdd4c91775bb14e3d2
Dagorik <dagorik@gmail.com>
2016-09-09 20:50:14 -0500

    Agregamos un fragmentos y las preguntas.

--- app/src/main/java/com/dev/dagorik/quizapp/StarQuizActivity.java
+++ app/src/main/java/com/dev/dagorik/quizapp/StarQuizActivity.java
@@ -1,26 +1,95 @@
 package com.dev.dagorik.quizapp;
 
+import android.content.Intent;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.dev.dagorik.quizapp.fragments.QuestionFragment;
 
-public class StarQuizActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class StarQuizActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private List<String> mListPreguntas = new ArrayList<>();
+    private List<Integer> mListRespuestas = new ArrayList<>();
+    private QuestionFragment questionFragment;
+    private ImageView iv_left, iv_right;
+
+    private int questionPosition;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_star_quiz);
 
-        QuestionFragment questionFragment= new QuestionFragment("¿De a como?");
+        initPreguntas();
+        initRespuestas();
+        cambiarFragment(0);
+
+    }
+
+    private void cambiarFragment(int position) {
+
+        questionFragment = new QuestionFragment(mListPreguntas.get(position));
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction()
-                .replace(R.id.frameLayaut,questionFragment)
+                .replace(R.id.frameLayaut, questionFragment)
                 .commit();
 
     }
 
+    private void initPreguntas() {
+        mListPreguntas.add("¿Chevrolet es autor del Chevelle?");
+        mListPreguntas.add("¿Ford tiene el motor 350?");
+        mListPreguntas.add("¿Mopar es mejor que los chevys?");
+        mListPreguntas.add("¿SuperSport es de Chevrolet?");
+        mListPreguntas.add("¿Charger es Mopar?");
+        mListPreguntas.add("¿Existe barracuda SS?");
+        mListPreguntas.add("¿El mejor motor de ford es el 351?");
+        mListPreguntas.add("¿Big block es considerado desde el 400?");
+        mListPreguntas.add("¿Slan-Six es de Chevrolet?");
 
+    }
+
+    private void initRespuestas() {
+        mListRespuestas.add(1);
+        mListRespuestas.add(0);
+        mListRespuestas.add(0);
+        mListRespuestas.add(1);
+        mListRespuestas.add(1);
+        mListRespuestas.add(0);
+        mListRespuestas.add(1);
+        mListRespuestas.add(1);
+        mListRespuestas.add(0);
+
+    }
+
+    private void initView() {
+        //Declarando
+        iv_left= (ImageView) findViewById(R.id.iv_left);
+        iv_right= (ImageView) findViewById(R.id.iv_right);
+
+        //El escuchador de los botones
+        iv_left.setOnClickListener(this);
+        iv_right.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+
+            case R.id.iv_left:
+                //No hace nada por el momento position =0
+                break;
+            case R.id.iv_right:
+
+            break;
+        }
+    }
 }
--- app/src/main/java/com/dev/dagorik/quizapp/fragments/QuestionFragment.java
+++ app/src/main/java/com/dev/dagorik/quizapp/fragments/QuestionFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.dev.dagorik.quizapp.R;
@@ -13,11 +14,12 @@ import com.dev.dagorik.quizapp.R;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class QuestionFragment extends Fragment {
+public class QuestionFragment extends Fragment implements View.OnClickListener {
 
-
+    //Declarando
     private String title;
     private TextView tvTitle;
+    private Button btn_true, btn_false;
 
     public QuestionFragment(String title) {
         this.title = title;
@@ -34,10 +36,33 @@ public class QuestionFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_question, container, false);
 
+//      Referenciando todo
         tvTitle = (TextView) view.findViewById(R.id.tvTitle);
+//      Cambiando el titutlo de la pregunta
         tvTitle.setText(title);
+        btn_true = (Button) view.findViewById(R.id.btn_true);
+        btn_false = (Button) view.findViewById(R.id.btn_false);
+
+//      Escuchando
+        btn_false.setOnClickListener(this);
+        btn_true.setOnClickListener(this);
+
 
         return view;
     }
 
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_true:
+                btn_true.setBackgroundResource(R.color.colorTrue);
+                btn_false.setBackgroundResource(R.color.colorAmbar);
+                break;
+
+            case R.id.btn_false:
+                btn_false.setBackgroundResource(R.color.colorFalse);
+                btn_true.setBackgroundResource(R.color.colorAmbar);
+                break;
+        }
+    }
 }
